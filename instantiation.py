6b1d5dde29f970a4971d4798d016ac268af8bf5d
SMAJL-master <51054114+CS-BIO@users.noreply.github.com>
2023-11-21 22:05:53 +0800

    Update instantiation.py

--- instantiation.py
+++ instantiation.py
@@ -5,6 +5,6 @@ from layer import CGCN
 def Create_model(args):
 
     model = CGCN(aggregator=args.aggregator, feature=args.dimensions, hidden1=args.hidden1, hidden2=args.hidden2, decoder1=args.decoder1, dropout=args.dropout)
-    optimizer = Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    optimizer = Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay) 
 
     return model, optimizer
